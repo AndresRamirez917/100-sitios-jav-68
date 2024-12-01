73808c1641dd68451015fc5189a244be3f209e30
AndresRamirez917 <andresmintic2021@gmail.com>
2024-12-01 15:09:29 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+async function getData() {
+    const result = await fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    const coctail = await result.json();
+    const randomDrinks = coctail.drinks.sort(()=>0.5 - Math.random()).slice(0,6)
+    coctail.drinks.forEach(element => {
+        const box = document.createRange().createContextualFragment(`
+            
+                <div class="flex-box-1 box-1">
+                    <img src="${element.strDrinkThumb}" alt="">
+                    <h3>Learning Web</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores molestias esse a illum sequi, tenetur consequuntur perspiciatis quisquam! Accusamus nulla sed atque, saepe nam dolor.</p>
+                    <a href="">Learn more</a>
+                </div>
+            
+            `)
+            const my_workrows = document.querySelector('.mywork-row');
+            my_workrows.append(box)
+    });
+}
+
+getData()
